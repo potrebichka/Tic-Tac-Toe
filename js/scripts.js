b4d2d118bc5b2f0d34e0a5f2ed4b0ea31214c748
Nina Potrebich and Uriel Gonzalez <uriel@>
2019-11-06 16:21:39 -0800

    Add full refactored code.

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,6 @@ Space.prototype.mark = function(value) {
     return false;
   }
   this.value = value;
-  console.log(this.value);
   return true;
 }
 
@@ -63,65 +62,61 @@ Board.prototype.initializeBoard = function() {
   }
 }
 
-// function valueTracker (track) {
-//   if (track[0].value === track[1].value && track[0].value === track[2].value && track[1].value === track[2].value && track[0].value !== EMPTY) {
-//     return track[0].value;
-//   }
-//   return false
-// }
+Board.prototype.checkIfFull = function() {
+  for (var i=0; i<this.spaces.length; i++) {
+    if (this.spaces[i].isEmpty()) {
+      return false
+    }
+  }
+  return true
+}
+
+function valueTracker (track) {
+  if (track[0].value === track[1].value && track[0].value === track[2].value && track[1].value === track[2].value && track[0].value !== EMPTY) {
+    return track[0].value;
+  }
+  return false
+}
+
 
 Board.prototype.hasWon = function() {
 
 //Rows
   var row1 = this.spaces.slice(0, 3);
-  console.log(row1);
-  if (row1[0].value === row1[1].value && row1[0].value === row1[2].value && row1[1].value === row1[2].value && row1[0].value !== EMPTY) {
-    return row1[0].value;
-  }
+  var result = valueTracker(row1);
 
   var row2 = this.spaces.slice(3, 6);
-  console.log(row2);
-  if (row2[0].value === row2[1].value && row2[0].value === row2[2].value && row2[1].value === row2[2].value && row2[0].value !== EMPTY) {
-    return row2[0].value;
-  }
+  result = result || valueTracker(row2);
 
   var row3 = this.spaces.slice(6, 9);
-  console.log(row3);
-  if (row3[0].value === row3[1].value && row3[0].value === row3[2].value && row3[1].value === row3[2].value && row3[0].value !== EMPTY) {
-    return row3[0].value;
-  }
+  result = result || valueTracker(row3);
 
 //Columns
   var column1 = [this.spaces[0], this.spaces[3], this.spaces[6]];
-  console.log('col1', column1);
-  if (column1[0].value === column1[1].value && column1[0].value === column1[2].value && column1[1].value === column1[2].value && column1[0].value !== EMPTY) {
-    return column1[0].value;
-  }
+  result = result || valueTracker(column1);
 
   var column2 = [this.spaces[1], this.spaces[4], this.spaces[7]];
-  console.log('col2',column2);
-  if (column2[0].value === column2[1].value && column2[0].value === column2[2].value && column2[1].value === column2[2].value && column2[0].value !== EMPTY) {
-    return column2[0].value;
-  }
+  result = result || valueTracker(column2);
 
   var column3 = [this.spaces[2], this.spaces[5], this.spaces[8]];
-  console.log('col3',column3);
-    if (column3[0].value === column3[1].value && column3[0].value === column3[2].value && column3[1].value === column3[2].value && column3[0].value !== EMPTY) {
-    return column3[0].value;
-  }
+  result = result || valueTracker(column3);
+
 //Diagonals
   var diagonal1 = [this.spaces[0], this.spaces[4], this.spaces[8]];
-    if (diagonal1[0].value === diagonal1[1].value && diagonal1[0].value === diagonal1[2].value && diagonal1[1].value === diagonal1[2].value && diagonal1[0].value !== EMPTY) {
-    return diagonal1[0].value;
-  }
+  result = result || valueTracker(diagonal1);
 
   var diagonal2 = [this.spaces[2], this.spaces[4], this.spaces[6]];
-    if (diagonal2[0].value === diagonal2[1].value && diagonal2[0].value === diagonal2[2].value && diagonal2[1].value === diagonal2[2].value && diagonal2[0].value !== EMPTY) {
-    return diagonal2[0].value;
+  result = result || valueTracker(diagonal2);
+
+  if (this.checkIfFull()) {
+    if (result) {
+      return result
+    } else {
+      return "Tie"
+    }
   }
 
-  return false;
-
+  return result;
 }
 
 function Game() {
@@ -168,136 +163,160 @@ $(document).ready(function() {
     game.newGame();
   });
 
-  $("#1").click(function () {
-    var result = game.move(1);
+  $(".grid-item").click(function (event) {
+    // console.log(event.target.id);
+    var id = event.target.id;
+    var result = game.move(id);
       if (result) {
-        $("#1").text(result);
-        $("#player").text(result);
+        $("#" + id).text(result);
+        if (result === X) {
+            $("#player").text(O);
+        } else {
+          $("#player").text(X);
+        }
 
     var resultCondition = game.board.hasWon();
       if (resultCondition) {
+        $("#gameDisplay").hide();
         $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
+        if (resultCondition === "Tie") {
+          $("#winner").text("It is a tie!");
+        } else {
+          $("#winner").text("The winner is player " + resultCondition + "!");
+        }
       }
     }
   });
 
-  $("#2").click(function () {
-    var result = game.move(2);
-    if (result) {
-      $("#2").text(result);
-      $("#player").text(result);
+  // $("#1").click(function (event) {
+  //   console.log(event.target.id);
+  //   var result = game.move(1);
+  //     if (result) {
+  //       $("#1").text(result);
+  //       $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
 
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#3").click(function () {
-    var result = game.move(3);
-    if (result) {
-      $("#3").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#4").click(function () {
-    var result = game.move(4);
-    if (result) {
-      $("#4").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#5").click(function () {
-    var result = game.move(5);
-    if (result) {
-      $("#5").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#6").click(function () {
-    var result = game.move(6);
-    if (result) {
-      $("#6").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#7").click(function () {
-    var result = game.move(7);
-    if (result) {
-      $("#7").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#8").click(function () {
-    var result = game.move(8);
-    if (result) {
-      $("#8").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
-
-  $("#9").click(function () {
-    var result = game.move(9);
-    if (result) {
-      $("#9").text(result);
-      $("#player").text(result);
-
-    var resultCondition = game.board.hasWon();
-      if (resultCondition) {
-        $("#result").fadeIn();
-        $("#winner").text("The winner is player " + resultCondition + "!");
-      }
-    }
-  });
+  // $("#2").click(function () {
+  //   var result = game.move(2);
+  //   if (result) {
+  //     $("#2").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#3").click(function () {
+  //   var result = game.move(3);
+  //   if (result) {
+  //     $("#3").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#4").click(function () {
+  //   var result = game.move(4);
+  //   if (result) {
+  //     $("#4").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#5").click(function () {
+  //   var result = game.move(5);
+  //   if (result) {
+  //     $("#5").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#6").click(function () {
+  //   var result = game.move(6);
+  //   if (result) {
+  //     $("#6").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#7").click(function () {
+  //   var result = game.move(7);
+  //   if (result) {
+  //     $("#7").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#8").click(function () {
+  //   var result = game.move(8);
+  //   if (result) {
+  //     $("#8").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
+  //
+  // $("#9").click(function () {
+  //   var result = game.move(9);
+  //   if (result) {
+  //     $("#9").text(result);
+  //     $("#player").text(result);
+  //
+  //   var resultCondition = game.board.hasWon();
+  //     if (resultCondition) {
+  //       $("#result").fadeIn();
+  //       $("#winner").text("The winner is player " + resultCondition + "!");
+  //     }
+  //   }
+  // });
   $("#startOver").click(function () {
     $("#start").fadeIn();
     $(".grid-item").text("");
     $("#result").hide();
-    $("#gameDisplay").hide();
-
   })
 });
